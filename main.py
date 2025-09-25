55785ff5e539a54efe849c4fe46d9975dbb02a7a
Joana Silva. <joanadasilva.pr@gmail.com>
2025-09-25 10:36:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,3 @@ from bs4 import BeautifulSoup
 url="https://censo2022.ibge.gov.br/panorama/"
 resposta= requests.get(url)
 soup=BeautifulSoup(resposta.text, 'lxml')
-
-df = pd.read_csv('dados.csv', delimiter=';')
-total = df['População (pessoas)'].sum()
-df['Percentual'] = (df['População (pessoas)'] / total * 100).round(2)
-df.to_csv('analise_cor_raca.csv', index=False)
