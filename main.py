ba18c92c8317f2e45ded3303d792858c8d4b9f1e
Joana <joanadasilva.pr@gmail.com>
2025-09-25 10:36:40 -0300

    adicionei o que procura a idade no censo

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import time 
+from selenium import webdriver
 
 
-url="https://censo2022.ibge.gov.br/panorama/"
-resposta= requests.get(url)
-soup=BeautifulSoup(resposta.text, 'lxml')
+pesquisador=webdriver.Chrome()
+pesquisador.get("https://censo2022.ibge.gov.br/panorama/")
+time.sleep(3)
+soup=BeautifulSoup(pesquisador.page_source,"html.parser")
+
+tudo = soup.find('div', id="piramide-etaria", class_="grafico-piramideEtaria piramideV2")
+idades=[]
+
+if tudo:
+        parte_dos_itens=tudo.find_all('div',class_="item item-v1")
+        for i in parte_dos_itens:
+            texto=i.find('div', class_="legenda")
+            if texto:
+               texto= texto.text.strip()
+               idades.append(texto)
 
 
 
+dic={"Idade": idades}
+
+print(dic)
